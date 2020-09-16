e95ce4d895c3cb0ee5a4997ff044a52266f65c9d
Yoona8 <elena243v@gmail.com>
2020-09-16 14:40:25 +0300

    Add menu template

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
-console.log('It works!');
+'use strict';
+
+const getMenuTemplate = () => {
+  return `
+    <nav class="header__nav container">
+      <a href="index.html" class="header__logo logo">TaskManager</a>
+      <ul class="user-menu">
+        <li>
+          <a href="#">Add new task</a>
+        </li>
+      </ul>
+      <ul class="header__menu menu">
+        <li>
+          <a href="#">Tasks</a>
+        </li>
+        <li>
+          <a href="#">Statistics</a>
+        </li>
+      </ul>
+    </nav>
+  `;
+};
+
+const render = (container, template) => {
+  container.innerHTML = template;
+};
+
+const menuContainer = document.querySelector('.header');
+
+render(menuContainer, getMenuTemplate());
